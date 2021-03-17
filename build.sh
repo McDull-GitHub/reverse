1ef69f4c778a0c966959ee5fcf4a8fe25990cc91
Mcdull <30206217+McDull-GitHub@users.noreply.github.com>
2021-03-17 18:26:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 wget https://github.com/txthinking/mr2/releases/download/v20200102/mr2_linux_amd64
 wget https://github.com/XTLS/Xray-core/releases/download/v1.4.0/Xray-linux-64.zip && unzip Xray*
 ls -al
-uname -a
 chmod 777 mr2_linux_amd64
 chmod 777 xray
 curl ip.sb
+uname -r
+echo "net.core.default_qdisc=fq" | sudo tee --append /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee --append /etc/sysctl.conf
+sudo sysctl -p
 ./mr2_linux_amd64 client -s 42.193.185.108:11111 -p password -P 12345 -c 127.0.0.1:9909 &
 timeout 600 ./xray run -c config.json
